6071df9687ca14859a0597b6e70597e9800b7819
Zabil C.M <zabilcm@gmail.com>
2010-07-10 16:18:37 +0530

    Refactored to apply rules after adding rules

--- src/org/openmrs/module/infopathconverter/InfopathForm.java
+++ src/org/openmrs/module/infopathconverter/InfopathForm.java
@@ -14,9 +14,12 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import javax.xml.xpath.XPathExpressionException;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 public class InfopathForm {
@@ -30,38 +33,66 @@ public class InfopathForm {
     }
 
     public Document toPage(TemplateXml templateXml, InfopathXsd xsd) throws Exception {
-        Document rawDocument = XmlDocumentFactory.createXmlDocumentFromStream(new ByteArrayInputStream(content.getBytes()));
-        Node node = extractPageBody(rawDocument);
 
-        Document page = XmlDocumentFactory.createEmptyXmlDocument();
-        Element pageElement = page.createElement("page");
-        pageElement.setAttribute("title", formName);
-        page.appendChild(pageElement);
-        pageElement.appendChild(page.importNode(node, true));
 
-        extractBindings(page, templateXml, xsd);
-        return page;
+        XmlDocument page = new XmlDocument();
+        page.createPage(formName, new XmlDocument(content).getBody());
+
+        page.addRule("//*[starts-with(@xd:binding,'patient/')]", new PatientRule(page.getDocument()));
+        page.addRule("//*[starts-with(@xd:binding,'encounter/encounter.encounter_datetime') or contains(@xd:binding,'encounter/encounter.provider_id')]", new EncounterRule(page.getDocument()));
+        page.addRule("//*[starts-with(@xd:binding,'encounter/encounter.location_id')]", new EncounterLocationRule(page.getDocument()));
+        page.addRule("//*[starts-with(@xd:binding,'obs/')]", new ObservationRule(page.getDocument(), templateXml, xsd));
+        page.addRule("//input[@xd:CtrlId='SubmitButton']", new SubmitButtonRule(page.getDocument()));
+
+        page.applyRules();
+
+        return page.getDocument();
     }
 
-    public Node extractPageBody(Document document) throws XPathExpressionException {
-        NodeList matchNodes = XPathUtils.matchNodes(document, "//body").getNodes();
-        if (matchNodes.getLength() > 0) {
-            return matchNodes.item(0);
-        } else {
-            return document.createElement("body");
+
+    private class XmlDocument {
+        private Document document;
+        private HashMap<String, Rule> rules = new HashMap<String, Rule>();
+
+        public XmlDocument(String content) throws IOException, SAXException, ParserConfigurationException {
+            document = XmlDocumentFactory.createXmlDocumentFromStream(new ByteArrayInputStream(content.getBytes()));
+
         }
-    }
 
+        public XmlDocument() throws ParserConfigurationException {
+            document = XmlDocumentFactory.createEmptyXmlDocument();
 
-    private void extractBindings(Document document, TemplateXml templateXml, InfopathXsd xsd) throws Exception {
-        applyRules(document, "//*[starts-with(@xd:binding,'patient/')]", new PatientRule(document));
-        applyRules(document, "//*[starts-with(@xd:binding,'encounter/encounter.encounter_datetime') or contains(@xd:binding,'encounter/encounter.provider_id')]", new EncounterRule(document));
-        applyRules(document, "//*[starts-with(@xd:binding,'encounter/encounter.location_id')]", new EncounterLocationRule(document));
-        applyRules(document, "//*[starts-with(@xd:binding,'obs/')]", new ObservationRule(document, templateXml, xsd));
-        applyRules(document, "//input[@xd:CtrlId='SubmitButton']", new SubmitButtonRule(document));
-    }
+        }
 
-    private void applyRules(Document document, String query, Rule rule) throws Exception {
-        rule.apply(XPathUtils.matchNodes(document, query));
+        public Node getBody() {
+            NodeList matchNodes = XPathUtils.matchNodes(document, "//body").getNodes();
+            if (matchNodes.getLength() > 0) {
+                return matchNodes.item(0);
+            } else {
+                return document.createElement("body");
+            }
+        }
+
+        public void createPage(String title, Node body) {
+            Element pageElement = document.createElement("page");
+            pageElement.setAttribute("title", title);
+            document.appendChild(pageElement);
+            pageElement.appendChild(document.importNode(body, true));
+        }
+
+        public Document getDocument() {
+            return document;
+        }
+
+        public void addRule(String binding, Rule rule) {
+            rules.put(binding, rule);
+        }
+
+        public void applyRules() throws Exception {
+            for (String binding : rules.keySet()) {
+                Rule rule = rules.get(binding);
+                rule.apply(XPathUtils.matchNodes(document, binding));
+            }
+        }
     }
 }
